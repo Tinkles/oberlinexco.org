edc51e3ca705ddf2cbde61950a77b8f675ae5f5f
Malcolm Balch <malcolmbalch@malcolm-balchs-macbook-pro.local>
2012-07-02 19:26:06 -0500

    virgin rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OberlinexcoOrg::Application.config.secret_token = '5b8cbf0a5c761007898daad1e7164ba540a9201a6065cfb0bcdd841591938910c3c541a91abad8d0d9e5243949359bdf3a553af0dc8ca5f52591ca391cd555eb'
