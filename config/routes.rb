6d56e39ba659d1d2c605c88c6d94a9a16bd7d7b9
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-07-07 11:28:59 -0400

    First pass at default Devise user model.  No customization yet.  Should modify the model and migration to include T#, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 OberlinexcoOrg::Application.routes.draw do
+  devise_for :users
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'home#index'
