d1da89d45cf4d17628839b1659f815b8703d2baa
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-07-16 23:10:06 -0400

    added uniqueness validations to user model for email and T#

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :timeoutable
 
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :last_name, :first_name, :t_number
-  # attr_accessible :title, :body
+
+  # Validations
+  validates_uniqueness_of :email, :t_number
+
 end
