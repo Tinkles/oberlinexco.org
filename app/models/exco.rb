cb062fc4e7f157063e4071333eda2c0a5667a2cf
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-07-18 00:19:01 -0400

    added year, term, and enrollment limit to exco model and migration

--- app/models/exco.rb
+++ app/models/exco.rb
@@ -1,3 +1,14 @@
 class Exco < ActiveRecord::Base
-  attr_accessible :description, :name
+
+  attr_accessible :description, :name, :enrollment_limit, :year, :term
+
+  validate :term_is_valid
+
+  TERMS = ['Fall', 'Spring']
+
+  # validate to make sure that the given term is in TERMS constant
+  def term_is_valid
+    errors.add(:term, 'is not valid') unless TERMS.include? term
+  end
+
 end
--- db/migrate/20120717034150_create_excos.rb
+++ db/migrate/20120717034150_create_excos.rb
@@ -1,10 +1,16 @@
 class CreateExcos < ActiveRecord::Migration
   def change
     create_table :excos do |t|
+
       t.string :name
       t.text :description
+      t.integer :enrollment_limit
+
+      t.integer :year
+      t.string :term
 
       t.timestamps
+
     end
   end
 end
