f9587ff8547547ef78a91dcbe49e6cc56c3dfb1f
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-07-19 00:40:00 -0400

    fixtures and unit tests for excos done (and minor change to exco model syntax)

--- app/models/exco.rb
+++ app/models/exco.rb
@@ -16,7 +16,7 @@ class Exco < ActiveRecord::Base
 
   # validate to make sure that the given course_number does not duplicate within the same year and term
   def course_number_is_unique_on_year_and_term
-    others = Exco.where(:year => self.year, :term => self.term, :course_number => self.course_number)
+    others = Exco.where(year: self.year, term: self.term, course_number: self.course_number)
     unless others.empty?
       errors.add(:course_number, "conflicts with #{others.first.name}, #{self.term} #{self.year}") 
     end
--- test/unit/exco_test.rb
+++ test/unit/exco_test.rb
@@ -1,7 +1,67 @@
 require 'test_helper'
 
 class ExcoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  context "creating" do
+
+    setup do
+      @aikido_exco = Exco.new(name: "Aikido ExCo",
+                              course_number: 1,
+                              description: "Learn aikido.",
+                              enrollment_limit: 16,
+                              year: 2012,
+                              term: "Spring")
+    end
+
+    context "a valid exco" do
+      should "be valid" do
+        assert @aikido_exco.valid?
+      end
+    end
+
+    context "an exco with an empty description" do
+      setup do
+        @aikido_exco.description = nil
+      end
+      should "be valid" do
+        assert @aikido_exco.valid?
+      end
+    end
+
+    context "an empty exco" do
+      setup do
+        @empty_exco = Exco.new
+      end
+      should "be invalid and have errors in all required fields" do
+        assert @empty_exco.invalid?
+        assert @empty_exco.errors[:name].any?
+        assert @empty_exco.errors[:course_number].any?
+        assert @empty_exco.errors[:enrollment_limit].any?
+        assert @empty_exco.errors[:year].any?
+        assert @empty_exco.errors[:term].any?
+      end
+    end
+
+    context "an exco with a bad term" do
+      setup do
+        @aikido_exco.term = "Summer"
+      end
+      should "be invalid and have an error in the term field" do
+        assert @aikido_exco.invalid?
+        assert @aikido_exco.errors[:term].any?
+      end
+    end
+
+    context "an exco with the same course_number" do
+      setup do
+        @aikido_exco.course_number = 69
+      end
+      should "fail and have errors in course_number field" do
+        assert @aikido_exco.invalid?
+        assert @aikido_exco.errors[:course_number].any?
+      end
+    end
+
+  end
+
 end
