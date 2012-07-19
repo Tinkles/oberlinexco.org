b08cd318386535971ca6f6a85029da04a0f6212a
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-07-19 00:14:05 -0400

    User validates presence of email, first_name, last_name, t_number

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :timeoutable
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :last_name, :first_name, :t_number
 
   # Validations
   validates_uniqueness_of :email, :t_number
+  validates_presence_of :email, :last_name, :first_name, :t_number
 
 end
