315d09068b7946ff651eea65dfe0f64ceb3d5aec
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-08-15 21:37:55 -0400

    just reformatted user devise model arguments to more easily take in and out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :timeoutable, :confirmable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :timeoutable,
+         :confirmable
+         #:token_authenticatable,
+         #:confirmable,
+         #:lockable,
+         #:omniauthable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :last_name, :first_name, :t_number
 
