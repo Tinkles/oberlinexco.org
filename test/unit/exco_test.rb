b1bf1077d90f2e682fcbbb317dcf1f17673163a1
Isaac Hollander McCreery <ihmccreery@gmail.com>
2012-08-20 18:23:20 -0400

    disabled Grant on individual unit tests

--- test/unit/exco_test.rb
+++ test/unit/exco_test.rb
@@ -2,138 +2,150 @@ require 'test_helper'
 
 class ExcoTest < ActiveSupport::TestCase
 
-  context "creating" do
+  context "without Grant," do
 
     setup do
-      @aikido_exco = Exco.new(name: "Aikido ExCo",
-                              course_number: 1,
-                              description: "Learn aikido.",
-                              enrollment_limit: 16,
-                              year: 2012,
-                              term: "Spring",
-                              instructors: [users(:george), users(:abe)])
+      Grant::Status.disable_grant
     end
 
-    context "a valid exco" do
-      should "be valid" do
-        assert @aikido_exco.valid?
-      end
+    teardown do
+      Grant::Status.enable_grant
     end
 
-    context "an exco with an empty description" do
+    context "creating" do
+
       setup do
-        @aikido_exco.description = nil
+        @aikido_exco = Exco.new(name: "Aikido ExCo",
+                                course_number: 1,
+                                description: "Learn aikido.",
+                                enrollment_limit: 16,
+                                year: 2012,
+                                term: "Spring",
+                                instructors: [users(:george), users(:abe)])
       end
-      should "be valid" do
-        assert @aikido_exco.valid?
+
+      context "a valid exco" do
+        should "be valid" do
+          assert @aikido_exco.valid?
+        end
       end
+
+      context "an exco with an empty description" do
+        setup do
+          @aikido_exco.description = nil
+        end
+        should "be valid" do
+          assert @aikido_exco.valid?
+        end
+      end
+
+      context "an exco with no instructors" do
+        setup do
+          @aikido_exco.description = []
+        end
+        should "be valid" do
+          assert @aikido_exco.valid?
+        end
+      end
+
+      context "an empty exco" do
+        setup do
+          @empty_exco = Exco.new
+        end
+        should "be invalid and have errors in all required fields" do
+          assert @empty_exco.invalid?
+          assert @empty_exco.errors[:name].any?
+          assert @empty_exco.errors[:course_number].any?
+          assert @empty_exco.errors[:enrollment_limit].any?
+          assert @empty_exco.errors[:year].any?
+          assert @empty_exco.errors[:term].any?
+        end
+      end
+
+      context "an exco with a bad term" do
+        setup do
+          @aikido_exco.term = "Summer"
+        end
+        should "be invalid and have an error in the term field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:term].any?
+        end
+      end
+
+      context "an exco with a 0 enrollment_limit" do
+        setup do
+          @aikido_exco.enrollment_limit = 0
+        end
+        should "be invalid and have an error in the enrollment_limit field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:enrollment_limit].any?
+        end
+      end
+
+      context "an exco with a -1 enrollment_limit" do
+        setup do
+          @aikido_exco.enrollment_limit = -1
+        end
+        should "be invalid and have an error in the enrollment_limit field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:enrollment_limit].any?
+        end
+      end
+
+      context "an exco with the same name, year, and term" do
+        setup do
+          @aikido_exco.name = excos(:sexco).name
+          @aikido_exco.year = excos(:sexco).year
+          @aikido_exco.term = excos(:sexco).term
+        end
+        should "fail and have errors in name field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:name].any?
+        end
+      end
+
+      context "an exco with the same course_number, year, and term" do
+        setup do
+          @aikido_exco.course_number = excos(:sexco).course_number
+          @aikido_exco.year = excos(:sexco).year
+          @aikido_exco.term = excos(:sexco).term
+        end
+        should "fail and have errors in course_number field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:course_number].any?
+        end
+      end
+
+      context "an exco with nonexistent instructors" do
+        setup do
+          @aikido_exco.instructors = [User.new]
+        end
+        should "fail and have errors in instructors field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:instructors].any?
+        end
+      end
+
+      context "an exco given invalid t_numbers" do
+        setup do
+          @aikido_exco.instructors_attributes = {"0"=>{"t_number"=>"T0", "_destroy"=>"false"}}
+        end
+        should "fail and have errors in instructors field" do
+          assert @aikido_exco.invalid?
+          assert @aikido_exco.errors[:instructors].any?
+        end
+      end
+
     end
 
-    context "an exco with no instructors" do
-      setup do
-        @aikido_exco.description = []
-      end
-      should "be valid" do
-        assert @aikido_exco.valid?
-      end
-    end
+    context "getting" do
 
-    context "an empty exco" do
-      setup do
-        @empty_exco = Exco.new
+      context "current excos" do
+        should "return current excos" do
+          assert Exco.current == Exco.where(:year => Date.today.year, :term => (Date.today.month > 5 ? 'Fall' : 'Spring')).order(:course_number)
+        end
       end
-      should "be invalid and have errors in all required fields" do
-        assert @empty_exco.invalid?
-        assert @empty_exco.errors[:name].any?
-        assert @empty_exco.errors[:course_number].any?
-        assert @empty_exco.errors[:enrollment_limit].any?
-        assert @empty_exco.errors[:year].any?
-        assert @empty_exco.errors[:term].any?
-      end
-    end
 
-    context "an exco with a bad term" do
-      setup do
-        @aikido_exco.term = "Summer"
-      end
-      should "be invalid and have an error in the term field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:term].any?
-      end
-    end
-
-    context "an exco with a 0 enrollment_limit" do
-      setup do
-        @aikido_exco.enrollment_limit = 0
-      end
-      should "be invalid and have an error in the enrollment_limit field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:enrollment_limit].any?
-      end
-    end
-
-    context "an exco with a -1 enrollment_limit" do
-      setup do
-        @aikido_exco.enrollment_limit = -1
-      end
-      should "be invalid and have an error in the enrollment_limit field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:enrollment_limit].any?
-      end
-    end
-
-    context "an exco with the same name, year, and term" do
-      setup do
-        @aikido_exco.name = excos(:sexco).name
-        @aikido_exco.year = excos(:sexco).year
-        @aikido_exco.term = excos(:sexco).term
-      end
-      should "fail and have errors in name field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:name].any?
-      end
-    end
-
-    context "an exco with the same course_number, year, and term" do
-      setup do
-        @aikido_exco.course_number = excos(:sexco).course_number
-        @aikido_exco.year = excos(:sexco).year
-        @aikido_exco.term = excos(:sexco).term
-      end
-      should "fail and have errors in course_number field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:course_number].any?
-      end
-    end
-
-    context "an exco with nonexistent instructors" do
-      setup do
-        @aikido_exco.instructors = [User.new]
-      end
-      should "fail and have errors in instructors field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:instructors].any?
-      end
-    end
-
-    context "an exco given invalid t_numbers" do
-      setup do
-        @aikido_exco.instructors_attributes = {"0"=>{"t_number"=>"T0", "_destroy"=>"false"}}
-      end
-      should "fail and have errors in instructors field" do
-        assert @aikido_exco.invalid?
-        assert @aikido_exco.errors[:instructors].any?
-      end
-    end
-
-  end
-
-  context "getting" do
-
-    context "current excos" do
-      should "return current excos" do
-        assert Exco.current == Exco.where(:year => Date.today.year, :term => (Date.today.month > 5 ? 'Fall' : 'Spring')).order(:course_number)
-      end
     end
 
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,88 +2,100 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  context "creating" do
+  context "without Grant," do
 
     setup do
-      @ben = User.new(email: "ben@pres.com",
-                      last_name: "Benjamin",
-                      first_name: "Franklin",
-                      t_number: "T00000010",
-                      password: "akeyandlightning")
+      Grant::Status.disable_grant
     end
 
-    context "a valid user" do
-      should "be valid" do
-        assert @ben.valid?
-      end
+    teardown do
+      Grant::Status.enable_grant
     end
 
-    context "an empty user" do
+    context "creating" do
+
       setup do
-        @empty_user = User.new
+        @ben = User.new(email: "ben@pres.com",
+                        last_name: "Benjamin",
+                        first_name: "Franklin",
+                        t_number: "T00000010",
+                        password: "akeyandlightning")
       end
-      should "be invalid and have errors in all required fields" do
-        assert @empty_user.invalid?
-        assert @empty_user.errors[:email].any?
-        assert @empty_user.errors[:first_name].any?
-        assert @empty_user.errors[:last_name].any?
-        assert @empty_user.errors[:t_number].any?
+
+      context "a valid user" do
+        should "be valid" do
+          assert @ben.valid?
+        end
+      end
+
+      context "an empty user" do
+        setup do
+          @empty_user = User.new
+        end
+        should "be invalid and have errors in all required fields" do
+          assert @empty_user.invalid?
+          assert @empty_user.errors[:email].any?
+          assert @empty_user.errors[:first_name].any?
+          assert @empty_user.errors[:last_name].any?
+          assert @empty_user.errors[:t_number].any?
+        end
+      end
+
+      context "a user with a duplicate email" do
+        setup do
+          @ben.email = users(:george).email
+        end
+        should "be invalid and have an error in the email field" do
+          assert @ben.invalid?
+          assert @ben.errors[:email].any?
+        end
+      end
+
+      context "a user with a duplicate t_number" do
+        setup do
+          @ben.t_number = users(:george).t_number
+        end
+        should "be invalid and have an error in the t_number field" do
+          assert @ben.invalid?
+          assert @ben.errors[:t_number].any?
+        end
+      end
+
+      context "a user with a bad email" do
+        setup do
+          @ben.email = "badbadbad"
+        end
+        should "be invalid and have an error in the email field" do
+          assert @ben.invalid?
+          assert @ben.errors[:email].any?
+        end
+      end
+
+      context "an exco with a bad t_number" do
+        setup do
+          @ben.t_number = "00000000"
+        end
+        should "be invalid and have an error in the t_number field" do
+          assert @ben.invalid?
+          assert @ben.errors[:t_number].any?
+        end
+      end
+
+    end
+
+    context "a user" do
+      context "who is an admin" do
+        should "be admin" do
+          assert users(:exco).admin
+        end
+      end
+      context "who is not an admin" do
+        should "not be admin" do
+          assert !users(:abe).admin
+        end
       end
     end
 
-    context "a user with a duplicate email" do
-      setup do
-        @ben.email = users(:george).email
-      end
-      should "be invalid and have an error in the email field" do
-        assert @ben.invalid?
-        assert @ben.errors[:email].any?
-      end
-    end
-
-    context "a user with a duplicate t_number" do
-      setup do
-        @ben.t_number = users(:george).t_number
-      end
-      should "be invalid and have an error in the t_number field" do
-        assert @ben.invalid?
-        assert @ben.errors[:t_number].any?
-      end
-    end
-
-    context "a user with a bad email" do
-      setup do
-        @ben.email = "badbadbad"
-      end
-      should "be invalid and have an error in the email field" do
-        assert @ben.invalid?
-        assert @ben.errors[:email].any?
-      end
-    end
-
-    context "an exco with a bad t_number" do
-      setup do
-        @ben.t_number = "00000000"
-      end
-      should "be invalid and have an error in the t_number field" do
-        assert @ben.invalid?
-        assert @ben.errors[:t_number].any?
-      end
-    end
-
-  end
-
-  context "a user" do
-    context "who is an admin" do
-      should "be admin" do
-        assert users(:exco).admin
-      end
-    end
-    context "who is not an admin" do
-      should "not be admin" do
-        assert !users(:abe).admin
-      end
-    end
   end
 
 end
